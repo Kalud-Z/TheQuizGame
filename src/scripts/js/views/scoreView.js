a34256aa38da3f4e503f46724a7c643fd5ed35e5
Kalud.Z <khaled.fhm@live.fr>
2020-03-09 08:08:58 +0100

    score cool effect . implemented

--- src/scripts/js/views/scoreView.js
+++ src/scripts/js/views/scoreView.js
@@ -8,5 +8,35 @@ export const loadScorePage = () => {
 }
 
 export const displayFinalScore = (finalScore , maxScore) => {
-    elements.scoreOutput.innerText =  finalScore + '/' + maxScore;
+    const finalScoreInPercentage = (finalScore * 100) / maxScore;
+
+    var counter = 0;
+    const func = () => {
+        elements.scoreOutput.innerText =  counter + '%';
+        counter++;
+    }
+
+    setDeceleratingTimeout(func, 1 , finalScoreInPercentage);
 }
+
+
+
+// HELPING FUNCTIONS #################################################################################
+const setDeceleratingTimeout = (callback, factor, times) =>
+{
+    var internalCallback = function(tick, counter) {
+        return function() {
+            var limit;
+            if (--tick >= 0) {
+                if(times >= 50 && times <= 100) { limit = (20 * times)/100 ; } else { limit = (25 * times)/100 }
+                if(tick < limit) { counter = counter + 10 } else { counter++ }
+                console.log(counter);
+                window.setTimeout(internalCallback, counter * factor );
+                callback();
+            }
+        }
+    }(times, 0);
+
+    window.setTimeout(internalCallback, factor);
+};
+ 
